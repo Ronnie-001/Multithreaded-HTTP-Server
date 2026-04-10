8cb5d0f8ddd383304e7748ae24d98846a92a1610
Ronald <ronald.adesida@gmail.com>
2026-04-10 12:36:48 +0100

    add: findServerAddress() function to find the server address to bind() to based on criteria.

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,12 +1,19 @@
+#include <cstdio>
+#include <cstdlib>
 #include <netdb.h>
+#include <__stddef_null.h>
+#include <sys/socket.h>
+#include <iostream>
 
+#define MY_PORT "3490"
+#define BACKLOG 10
 
 class Server {
 
 private:
     int _status;
     
-    // member variables to hold the socket file descriptors 
+    // Member variables to hold the socket file descriptors.
     int _sock_fd;
     int _bind_fd;
     int _listen_fd;
@@ -18,8 +25,58 @@ private:
 
 public:
     // Constructor
-    Server() {}
+    Server() 
+    {
+        // Look for IPv4 or IPv6.
+        _hints.ai_family = PF_UNSPEC;
+        // Look server addresses that use TCP.
+        _hints.ai_socktype = SOCK_STREAM;
+        _hints.ai_flags = AI_PASSIVE;
+        
+        // Get the server addresses that match the hints.
+        _status = getaddrinfo(NULL, MY_PORT, &_hints, &_servinfo);
+    }
     
     // Destructor
     ~Server() {}
+    
+    /*
+     * Gets a list of server addresses to connect on using getaddrinfo() and 
+     * addrinfo _hints for specifying criteria to look for when scanning through.
+     * Returns the unique file descriptor _bind_fd.
+     */
+    int findServerAddress() 
+    {
+        if (_status != 0) {
+            fprintf(stderr, "[ERROR] getaddrinfo: %s\n", gai_strerror(_status));
+            exit(EXIT_FAILURE);
+        }
+        
+        // Try each found server address until successful bind()
+        for (_ptr = _servinfo; _ptr != NULL; _ptr = _ptr->ai_next) {
+
+            _sock_fd = socket(_ptr->ai_family, _ptr->ai_socktype, _ptr->ai_protocol);
+
+            if (_sock_fd != -1) {
+                std::cerr << "[LOGS] Failed to create endpoint using socket() on server address: " << _ptr->ai_addr << "Trying next..."<< "\n"; 
+                continue;     
+            }
+
+            _bind_fd = bind(_sock_fd, _ptr->ai_addr, _ptr->ai_addrlen);
+
+            if (_bind_fd == -1) {
+               std::cerr << "[LOGS] Failed to bind on server address: " << _ptr->ai_addr << "Trying next..."<< "\n"; 
+               continue;     
+            }
+            
+            // Exit loop once you have found a server address to connect on.
+            break;
+        }
+        
+        return _bind_fd;
+
+    }
 };
+
+
+
