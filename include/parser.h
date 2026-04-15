b56a85b9e4149304614a523e471684eb4c0480ea
Ronald <ronald.adesida@gmail.com>
2026-04-15 13:45:22 +0100

    chore: started implementtation of the HttpParser class.

--- include/parser.h
+++ include/parser.h
@@ -2,6 +2,9 @@
 #define PARSER_H
 
 #include <string>
+#include "request.h"
+
+#define BUFFER_SIZE 1024
 
 // Used for parsing incoming HTTP requests accepter from the TCP listener.
 class HttpParser 
@@ -9,23 +12,31 @@ class HttpParser
 private:
     // Used to check if the entire HTTP request has been recieved.
     bool _complete;
+    int _conn_fd;
     std::string _request;
+    std::string _start_line;
 
+    // The final request to be constructed.
+    Request _parsed_request;
 public:
     // Constructor 
-    HttpParser();
+    HttpParser(int fd);
 
     // Destructor
     ~HttpParser();
     
     // Returns the state of _request.
-    bool isRequestComplete() const;
-    
+    bool isRequestComplete() const; 
     /*
-     * Function used for appending data from the recv() system call
+     * Used for appending data from the recv() system call
      * and appending it to _request.
     */
-    void appendData();
+    void appendData(const char* buffer, int bytes);
+    
+    /* Used for extracting the start line from the HTTP request, which
+     * contains the method, resource path and HTTP version,
+     */
+    std::string extractStartLine();
     
     // Used for extracting the method used, i.e. GET, POST, PUT, etc.
     std::string extractMethod(); 
@@ -35,8 +46,6 @@ public:
     
     // Used for extracting the HTTP version being used.
     std::string extractVersion();
-
-     
 };
 
 #endif // ! PARSER_H
